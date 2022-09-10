b0f36f25184a84283939d7a21e3c39949d1cd3e0
XXIV <13811862+thechampagne@users.noreply.github.com>
2022-09-10 09:38:11 +0300

    Removed unused import

--- src/open.zig
+++ src/open.zig
@@ -17,8 +17,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-const std = @import("std");
-
 extern "C" fn open_that(path: [*c]const u8) c_int;
 
 extern "C" fn open_with(path: [*c]const u8, app: [*c]const u8) c_int;
